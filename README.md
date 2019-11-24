04147266d1dc4eb92d24ad98575069012ffa3898
johanneshoetter <57453680+johanneshoetter@users.noreply.github.com>
2019-11-24 23:24:42 +0100

    Finished imhre rebuild

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # HealthDataIntegration
  repository for the lecture Information Integration
+
+notes for reducing ihme:
+- gaining knowledge about diseases which one gets by smoking a lot -> filter ihme using those, in order to reduce the dataset: https://www.unitypoint.org/livewell/article.aspx?id=17ace3fc-fb01-45c3-8617-1beb81404fc4
+- filter values:
+-- = Esophageal cancer
+-- = Liver cancer
+-- LIKE diabetes
+-- LIKE heart
+-- = Ectopic pregnancy
+-- LIKE vision
+-- LIKE lung
+-- = Tuberculosis
+-- = Rheumatoid arthritis
+-- LIKE stroke
+-- = Bladder cancer
+-- = Cervical cancer
+-- LIKE hearing
+-- = Psoriaris
+-- LIKE dementia
---
+++ code/ihme.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+def rebuild_ihme(engine, connection):
+    # get all ihme tables that need to be united
+    sql = """
+    SELECT name
+    FROM sqlite_master
+    WHERE type = 'table'
+    AND name LIKE 'ihme_gbd%'
+    ;
+    """
+    df = pd.read_sql_query(sql, engine)
+    ihme_tables = df['name'].values
+
+    # filter values for the ihme tables
+    # only rows containing information about the following topics will be addressed
+    # reduces ihme content from 360 diseases to the 27 relevant for smoking
+    filter_values = [
+        "=          'Esophageal cancer'",
+        "=          'Liver cancer'",
+        "=          'Ectopic pregnancy'",
+        "=          'Tuberculosis'",
+        "=          'Rheumatoid arthritis'",
+        "=          'Bladder cancer'",
+        "=          'Cervical cancer'",
+        "=          'Psoriaris'",
+        "LIKE       '%diabetes%'",
+        "LIKE       '%heart%'",
+        "LIKE       '%vision%'",
+        "LIKE       '%lung%'",
+        "LIKE       '%stroke%'",
+        "LIKE       '%hearing%'",
+        "LIKE       '%dementia%'"
+    ]
+    sql = 'SELECT key AS cause_id, value AS disease FROM cause \nWHERE' \
+          + '\n   OR'.join([' value {}'.format(filter_value) for filter_value in filter_values]) \
+          + ';'
+    result_set = connection.execute(sql).fetchall()
+
+    # ihme tables will be filtered for the following cause ids
+    cause_ids = ', '.join([str(row[0]) for row in result_set])
+
+    # unneccessary values will be deleted from the lookup table, indicating which ihme tables won't be needed nomore
+    sql = 'DELETE FROM cause WHERE key NOT IN ({});'.format(cause_ids)
+    connection.execute(sql);
+
+    # build the sql string by concatenating SELECTs using UNION
+    sql = '\nUNION\n'.join(
+        ["SELECT * FROM {} WHERE cause_id IN ({})".format(table_name, cause_ids) for table_name in ihme_tables]
+    )
+    ihme_df = pd.read_sql_query(sql, engine)
+    ihme_df.to_sql('ihme_smoking_diseases', con=engine, if_exists='replace');
